a0d348cae1084060d9e76f7621c074705a651564
MelanAster <wzh3078@126.com>
2024-09-17 09:55:05 +0800

    v0.10

--- QuickSlash/lang/en.lua
+++ QuickSlash/lang/en.lua
@@ -1,24 +1,29 @@
-local GU = MAGatherUp
+local QS = QuickSlash
 
-GU.Lang = {
-  TITLE             = "GatherUp",
-  KEYBIND           = "Open/Close GU",
+QS.Lang = {
+  CHARACTER_SETTING =         "Configuration by Characters",
+  CREATE_QUICKSLOT =          "Create QuickSlots for Slash Commands",
+  WHEEL_CATEGORY =            "Wheel Category",
+  WHEEL_SLOT =                "Slot",
+  WHEEL_ICON =                "Preset Icons",
+  WHEEL_ICON_CUSTOM =         "Custom Icons",
+  WHEEL_ICON_CUSTOM_TOOLTIP = "Fill in the Path of Game Icons\r\nFormat: \r\n/esoui/art/lfg/lfg_any_up_64.dds",
+  WHEEL_NAME =                "Title",
+  WHEEL_SLASH =               "Slash Commands",
+  WHEEL_SLASH_TOOLTIP =       "Recommended to Complete the Command in the Chat Bar first\r\nFormat: \r\n/cmx\r\n/dsst opt\r\n/script ...",
+  WHEEL_CHECK =               "Check Commands",
+  WHEEL_CHECK_TOOLTIP =       "Please Check the Attached Parameters by yourself",
+  WHEEL_APPLY =               "Add",
+  WHEEL_INFO =                "\r\n* QS Overwrites these Wheels without modifying Placed Items",
+  WHEEL_DESC =                "Configured: ",
+  WHEEL_EDIT =                "Edit",
+  WHEEL_EMPTY =               "Empty this Category",
+  WHEEL_DELETE =              "Delete this slot",
   
-  BUTTON_QUEUE_RUN  = "AutoRun",
-  BUTTON_QUEUE_RUN_TOOLISH = "(Left-Click) Single\r\n(Right-Click) Loop Mode\r\n* Loop Mode may cause disconnection over time",
-  BUTTON_QUEUE_STOP = "Stop",
-  BUTTON_QUEUE_NL   = "Not Leader",
+  STATUS_BAD_COMMAND =        "|cFF0000Invalid command!|r",
+  STATUS_GOOD_COMMAND =       "|c008000Good!|r",
+  STATUS_NO_NAME =            "|cFF0000Title Required!|r",
+  STATUS_ADDED =              "|c008000Already Added!|r",
   
-  BUTTON_EMPTY      = "Empty",
-  
-  EDITBOX_PASTE     = "Paste the Contents with @Username Here",
-  BUTTON_ANALYZE    = "Analyze",
-  
-  BUTTON_SAVELOAD   = "S/L Mode",
-  BUTTON_DELETE     = "Delete Mode",
-  BUTTON_FREESLOT   = "Free Slot",
-  
-  CHAT_START        = "Start!",
-  CHAT_DONE         = "Done!",
-  CHAT_CHANGED      = "Character Change Detected",
+  INVALID_SLASH =             "[QS] Invalid Commands!"
 }
